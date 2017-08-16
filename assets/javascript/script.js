3570a2631ee20171737f4200c84903f9aa3bdff3
Fox <michaelscottfox1@gmail.com>
2017-08-16 12:15:43 -0700

    minor styling changes and empty results container before making new search:

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   var API_KEY= '57de50e67dea471ca5f751740f3ded8e';
 
   $('#search').click(function() {
+    $('.results').empty();
     var searchTerm = $('#searchTerm').val();
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + searchTerm + "&api_key=" + API_KEY + "&limit=20";
 
@@ -16,6 +17,7 @@ $(document).ready(function() {
         var imgSRC = response.data[i].images.fixed_height.url;
         var embeddedGif = $('<img>');
         embeddedGif.attr('src', imgSRC);
+        embeddedGif.addClass('resultGif');
         $('.results').append(embeddedGif);
       }
 
