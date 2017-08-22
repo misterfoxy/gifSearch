58c217b1a4c10dd8d9c1b3df2dd703dd2732047d
Michael Fox <michaelscottfox1@gmail.com>
2017-08-21 23:31:58 -0700

    added onClick functionality to pause and resume gifs

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,10 @@
 $(document).ready(function() {
 
   var API_KEY= '57de50e67dea471ca5f751740f3ded8e';
+  var animateURL;
+  var imgSRC;
+  var stillURL;
+  var dataState;
 
   $('#search').click(function() {
     $('.results').empty();
@@ -11,15 +15,39 @@ $(document).ready(function() {
       url:queryURL,
       method: "GET"
     }).done(function(response){
-
+console.log(response);
 
       for (var i = 0; i<response.data.length;i++){
-        var imgSRC = response.data[i].images.fixed_height.url;
+        dataState = "still";
+        imgSRC = response.data[i].images.fixed_height_still.url;
+        stillURL = response.data[i].images.fixed_height_still.url;
+        animateURL = response.data[i].images.fixed_height.url;
         var embeddedGif = $('<img>');
         embeddedGif.attr('src', imgSRC);
+        embeddedGif.attr('data-animate', animateURL);
+        embeddedGif.attr('data-still', stillURL);
+        embeddedGif.attr('data-state', dataState);
         embeddedGif.addClass('resultGif');
+
         $('.results').append(embeddedGif);
       }
+
+
+      $('.resultGif').on("click", function(){
+
+        var state = $(this).attr('data-state');
+        if (state === 'still'){
+          $(this).attr('src', $(this).attr("data-animate"));
+          $(this).attr('data-state', "animate");
+        }
+
+        else {
+          $(this).attr('src', $(this).attr('data-still'));
+          $(this).attr('data-state', 'still');
+        }
+
+
+      });
 
 
     });
